2e3834c0bbe67c2bac8a0ee14efdb7569b3e1d88
Angry Boy <suvxdas@gmail.com>
2022-11-25 10:43:53 +0530

    h

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,5 +1,34 @@
-const square = (n) => {
-    console.log(n*n)
+const person = {
+    firstname: 'asish',
+    lastname: 'saddhi',
+    age: 250,
+    country:'finland',
+    city:"halsinki",
+    skills: [
+        'html',
+        'css',
+        'javascript',
+        'react',
+        'mongodb',
+        'python'
+    ],
+    getFullName: function() {
+        return `${this.firstname}${this.lastname}`
+    },
+    'phone number': "+34358585"
 }
 
-square(1)
+console.log(person.firstname)
+console.log(person['firstname'])
+console.log(person.country)
+console.log(person['phone number'])
+console.log(person.skills)
+console.log(person.getFullName())
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
